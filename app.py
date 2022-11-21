f0f8501dbf96d2afd48c353f3207757418047ed0
student <student@henson.com>
2022-11-21 19:29:23 +0100

    Add fetching rates with requests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # a dict for mock currencies EUR: 4.75
 import argparse
+import requests
+
 
 CURRENCIES = {
     'EUR': 4.75,
@@ -22,10 +24,17 @@ def parser():
 
 args = parser()
 
+def fetch_currency_rate(currency: str) -> float:
+    r = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/')
 
+    rates = r.json()[0]["rates"]
+
+    for i in rates:
+        if i["code"] == currency:
+            return i["mid"]
 
 def convert(value: float, currency: str) -> float:
-    exchange_rate = CURRENCIES[currency]
+    exchange_rate = fetch_currency_rate(currency)
     return value * exchange_rate
     # return exchange_rate * value
 
@@ -34,3 +43,6 @@ def convert(value: float, currency: str) -> float:
 input = (args.amount, args.currency)
 output = convert(value=input[0], currency=input[1])
 print(f"{output} PLN")
+
+
+
